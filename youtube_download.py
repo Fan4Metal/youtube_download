56a2fa0f41890acfb1c86ec3c2ee93a180e67c65
Fan4_Metal <fan4_metal@mail.ru>
2026-02-11 11:56:39 +0300

    Выбор битрейта mp3 и галка после конвертации

--- youtube_download.py
+++ youtube_download.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import yt_dlp
 
 
-def download_mp3(links_file: str, concurrent_fragments: int = 10, out_dir: str = "."):
+def download_mp3(links_file: str, concurrent_fragments: int = 10, out_dir: str = ".", bitrate: int = 192):
     errors = 0
     ok = 0
 
@@ -32,7 +32,7 @@ def download_mp3(links_file: str, concurrent_fragments: int = 10, out_dir: str =
             print()  # перенос строки после прогресса
             base, _ext = os.path.splitext(d.get("filename") or last_filename)
             final_mp3 = os.path.basename(base + ".mp3")
-            print(f"✅ {final_mp3}")
+            print(f"{final_mp3}", end="", flush=True)
 
     ydl_opts = {
         "format": "bestaudio/best",
@@ -45,7 +45,7 @@ def download_mp3(links_file: str, concurrent_fragments: int = 10, out_dir: str =
         "ignoreerrors": True,
         "progress_hooks": [hook],
         "postprocessors": [
-            {"key": "FFmpegExtractAudio", "preferredcodec": "mp3", "preferredquality": "192"},
+            {"key": "FFmpegExtractAudio", "preferredcodec": "mp3", "preferredquality": f"{bitrate}"},
             {"key": "FFmpegMetadata"},
         ],
         "noplaylist": False,
@@ -67,6 +67,7 @@ def download_mp3(links_file: str, concurrent_fragments: int = 10, out_dir: str =
             ret = ydl.download([url])
 
         if ret == 0:
+            print(" ✅")
             ok += 1
         else:
             errors += 1
@@ -80,4 +81,4 @@ if __name__ == "__main__":
         print(f"Файл с ссылками не найден: {file_to_download}")
         sys.exit(1)
     now = datetime.now().strftime("%d.%m.%Y")
-    download_mp3(file_to_download, out_dir=os.path.join("downloads", now))
+    download_mp3(file_to_download, out_dir=os.path.join("downloads", now), bitrate=192)
